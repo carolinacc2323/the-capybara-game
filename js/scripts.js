11cf42f27b3361def426852a5de54e6258858a35
naomitandem <carolinacc.tandem@gmail.com>
2024-04-12 11:00:19 +0200

    actualización

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,14 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const audioControl = document.getElementById('audio-control');
+const audioPlayer = document.querySelector('.audio-player');
+
+audioControl.addEventListener('click', () => {
+if (audioPlayer.paused) {
+    audioPlayer.play();
+} else {
+    audioPlayer.pause();
+}
+});
